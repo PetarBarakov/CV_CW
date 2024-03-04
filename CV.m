d9a64be9430c41bad5546e6a343f570330be06ef
Petar Barakov <barakov.petar@gmail.com>
2024-03-04 17:57:45 +0000

    add task 1 photos of multimeter and grid

--- CV.m
+++ CV.m
@@ -4,7 +4,7 @@
 
 %% ----------------TASK 3: Camera Calibration ---------------------------
 calibration_square_size = 2.2;      %2.2cm on A4 paper
-
+cameraCalibrator("source_images\FD_images_2\", calibration_square_size);
 
 
 %% ----------------Task 4: Transformation estimation --------------------
