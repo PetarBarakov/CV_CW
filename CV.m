3bb2be1c402def950192009bbb02881866a2d1ca
Petar Barakov <barakov.petar@gmail.com>
2024-03-04 14:32:29 +0000

    Add matlab calibration grid

--- CV.m
+++ CV.m
@@ -1,1 +1,15 @@
-## -----------
+%% ----------------TASK 2: Keypoint corrspondences between images ----------
+
+
+
+%% ----------------TASK 3: Camera Calibration ---------------------------
+calibration_square_size = 2.2;      %2.2cm on A4 paper
+
+
+
+%% ----------------Task 4: Transformation estimation --------------------
+
+
+
+
+%% ----------------Task 5: 3D Geometry ----------------------------------
