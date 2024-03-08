7a9a32ad4ebb2caa2e42af55ec1e0e7878aef64f
arthika <arthika220602@gmail.com>
2024-03-08 19:48:27 +0000

    add to Task 4

--- CV.m
+++ CV.m
@@ -1,45 +1,95 @@
-%% ----------------TASK 2: Keypoint corrspondences between images ----------
 clear all
 close all
 clc
 
-img1 = imread("source_images\no_grid\MicrosoftTeams-image (1).png");
-img2 = imread("source_images\no_grid\MicrosoftTeams-image (2).png");
+%% TASK 2: Keypoint Corrspondences Between Images 
+img1 = imread("source_images\HG_no_grid\WhatsApp Image 2024-03-08 at 18.01.55 (1).jpeg");
+img2 = imread("source_images\HG_no_grid\WhatsApp Image 2024-03-08 at 18.01.55 (2).jpeg");
 
-img1_grey = transpose(im2gray(img1));
-img2_grey = transpose(im2gray(img2));
+img1_grey = im2gray(img1);
+img2_grey = im2gray(img2);
 
-%------------------ Feature Detection ---------
+% Automatic Feature Detection 
 corners_im1 = detectSURFFeatures(img1_grey);
 corners_im2 = detectSURFFeatures(img2_grey);
 
-% NOTE: Algorithms that work well on Image (1) and Image (6) are: Harris, MinEigen,
-%       ORB (works quite well for features inside of the object)
+% NOTE: Algorithms that work well on Image (1) and Image (6) are: Harris, 
+% MinEigen, ORB (works quite well for features inside of the object)
 
 [features_im1, feature_index_im1 ] = extractFeatures(img1_grey, corners_im1);
 [features_im2, feature_index_im2 ] = extractFeatures(img2_grey, corners_im2);
 
-feature_paris = matchFeatures(features_im1, features_im2);
+feature_pairs = matchFeatures(features_im1, features_im2);
 
-matched_poitns_im1 = feature_index_im1(feature_paris(:, 1), :);
-matched_poitns_im2 = feature_index_im2(feature_paris(:, 2), :);
+matched_points_im1 = feature_index_im1(feature_pairs(:, 1), :);
+matched_points_im2 = feature_index_im2(feature_pairs(:, 2), :);
 
 figure;
-showMatchedFeatures(img1_grey, img2_grey, matched_poitns_im1, matched_poitns_im2, "montag")
+showMatchedFeatures(img1_grey, img2_grey, matched_points_im1, matched_points_im2, "montag")
 
-% %Manual feature point selection
+% Manual Feature Detection 
 % [fixedPoints_img1, movingPoints_img2] = cpselect(img1, img2, 'Wait', true) 
 % showMatchedFeatures(img1, img2, fixedPoints_img1, movingPoints_img2, "montag")
 
-%% ----------------TASK 3: Camera Calibration ---------------------------
-calibration_square_size = 2.2;      %2.2cm on A4 paper
+%% TASK 3: Camera Calibration 
+calibration_square_size = 2.2;      % 2.2cm on A4 paper
 % cameraCalibrator("source_images\all_1D_grid\", calibration_square_size);
-cameraCalibrator("source_images\FD_1D_grid\", calibration_square_size);
+% cameraCalibrator("source_images\1d_grid\", calibration_square_size);
 
 
-%% ----------------Task 4: Transformation estimation --------------------
+%% TASK 4: Transformation Estimation: Homography
+img1_HG = imread("source_images\HG_no_grid\WhatsApp Image 2024-03-08 at 18.01.55 (1).jpeg");
+img2_HG = imread("source_images\HG_no_grid\WhatsApp Image 2024-03-08 at 18.01.55 (2).jpeg");
 
+% The code below gives an estimation for the AFFINE transformation and not
+% the HOMOGRAPHY matrix. Googling seems that we have to solve the following 
+% equation where x' = Hx where x and x' take the form [x, y, 1]. 
+% This then gives us a system of equations we can solve to get our
+% parameters of H. To solve we need at least 4 matched points. 
 
+% There are also references to using the Direct Linear Transformation
+% https://medium.com/@insight-in-plain-sight/estimating-the-homography-matrix-with-the-direct-linear-transform-dlt-ec6bbb82ee2b
+% https://www.youtube.com/watch?v=Hi9EFtzPl8Y
 
+% [tform,inlierIdx] = estgeotform2d(matched_points_im1, matched_points_im2,"similarity");
+% inlier_pts_im1 = matched_points_im1(inlierIdx,:);
+% inlier_pts_im2  = matched_points_im2(inlierIdx,:);
+% 
+% showMatchedFeatures(img1_grey, img2_grey, inlier_pts_im1, inlier_pts_im2)
+% title("Matched Inlier Points")
+% 
+% outputView = imref2d(size(img1_grey));
+% recovered = imwarp(img2_grey,tform,OutputView=outputView);
+% 
+% figure, imshowpair(img1_grey,recovered,'montage')
 
-%% ----------------Task 5: 3D Geometry ----------------------------------
+%% TASK 4: Transformation Estimation: Fundamental
+img1_FD = imread("source_images\FD_no_grid\WhatsApp Image 2024-03-08 at 18.14.06 (1).jpeg");
+img2_FD = imread("source_images\FD_no_grid\WhatsApp Image 2024-03-08 at 18.14.06 (2).jpeg");
+
+img1_FD = im2gray(img1_FD);
+img2_FD = im2gray(img2_FD);
+
+% We have to solve the following  equation where x'Fx = 0 where x and x' 
+% take the form [x, y, 1]. This then gives us a system of equations we can 
+% solve to get our parameters of F. 
+% Useful Link: https://uk.mathworks.com/help/vision/ref/epipolarline.html
+% Link shows how to calculate fundamental matrix, epipoles and epipolar
+% lines 
+
+% Automatic Feature Detection 
+corners_im1 = detectSURFFeatures(img1_FD);
+corners_im2 = detectSURFFeatures(img2_FD);
+
+[features_im1, feature_index_im1 ] = extractFeatures(img1_grey, corners_im1);
+[features_im2, feature_index_im2 ] = extractFeatures(img2_grey, corners_im2);
+
+feature_pairs = matchFeatures(features_im1, features_im2);
+
+matched_points_im1 = feature_index_im1(feature_pairs(:, 1), :);
+matched_points_im2 = feature_index_im2(feature_pairs(:, 2), :);
+
+% Estimate Fundamental Matrix - NOT WORKING
+F = estimateFundamentalMatrix(matched_points_im1, matched_points_im2)
+
+%% TASK 5: 3D Geometry 
