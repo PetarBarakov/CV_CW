7eb66c17eb5ac7be0396f062baa8a2c2de3fd649
Petar Barakov <barakov.petar@gmail.com>
2024-03-08 17:24:36 +0000

    add images with the two figures and improved feature detection

--- CV.m
+++ CV.m
@@ -3,15 +3,15 @@ clear all
 close all
 clc
 
-img1 = imread("source_images\FD_images\Image (1).jpg");
-img2 = imread("source_images\FD_images\Image (6).jpg");
+img1 = imread("source_images\no_grid\MicrosoftTeams-image (1).png");
+img2 = imread("source_images\no_grid\MicrosoftTeams-image (2).png");
 
-img1_grey = im2gray(img1);
-img2_grey = im2gray(img2);
+img1_grey = transpose(im2gray(img1));
+img2_grey = transpose(im2gray(img2));
 
 %------------------ Feature Detection ---------
-corners_im1 = detectORBFeatures(img1_grey);
-corners_im2 = detectORBFeatures(img2_grey);
+corners_im1 = detectSURFFeatures(img1_grey);
+corners_im2 = detectSURFFeatures(img2_grey);
 
 % NOTE: Algorithms that work well on Image (1) and Image (6) are: Harris, MinEigen,
 %       ORB (works quite well for features inside of the object)
@@ -25,14 +25,16 @@ matched_poitns_im1 = feature_index_im1(feature_paris(:, 1), :);
 matched_poitns_im2 = feature_index_im2(feature_paris(:, 2), :);
 
 figure;
-showMatchedFeatures(img1_grey, img2_grey, matched_poitns_im1, matched_poitns_im2)
+showMatchedFeatures(img1_grey, img2_grey, matched_poitns_im1, matched_poitns_im2, "montag")
 
-%Manual feature point selection
-cpselect(img1, img2) %--TODO: Save the datapoints ams showMatchedFeatures
+% %Manual feature point selection
+% [fixedPoints_img1, movingPoints_img2] = cpselect(img1, img2, 'Wait', true) 
+% showMatchedFeatures(img1, img2, fixedPoints_img1, movingPoints_img2, "montag")
 
 %% ----------------TASK 3: Camera Calibration ---------------------------
 calibration_square_size = 2.2;      %2.2cm on A4 paper
-cameraCalibrator("source_images\FD_images\", calibration_square_size);
+% cameraCalibrator("source_images\all_1D_grid\", calibration_square_size);
+cameraCalibrator("source_images\FD_1D_grid\", calibration_square_size);
 
 
 %% ----------------Task 4: Transformation estimation --------------------
